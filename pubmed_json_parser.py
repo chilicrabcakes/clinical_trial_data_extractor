c53816d1e064d50fee349810d7c525e297a8fe85
Ayush Lall <ayushlall@g.ucla.edu>
2023-05-17 14:56:57 +0530

    Fixed a bug in PubmedJSONParser

--- pubmed_json_parser.py
+++ pubmed_json_parser.py
@@ -2,16 +2,21 @@ import requests
 import json
 
 class PubmedJSONParser():
-    def __init__(self, url, pmc_id):
-        self.pmc_id = pmc_id
-        self.url = url.format(self.pmc_id)
+    def __init__(self, config):
+        self.pmc_id = self.pmc_id = config['pmc_id']
+        self.url = config['pmc_url']
+        self.output_path = config['raw_text_path']
+        print('*'*100)
+        print('Parsing data from Pubmed')
+        print('*'*100)
         self.fetch_json()
         self.format_json()
         self.clean_formatted_json()
+        self.save_to_file()
         
     def fetch_json(self):
         print('Fetching data from PMC API...', end='')
-        resp = requests.get(self.url)
+        resp = requests.get(self.url.format(self.pmc_id))
         if resp.status_code == '404':
             raise ValueError('404 response from PMC API. Is the PMC id given correct?')
         self.data = resp.json()
@@ -21,27 +26,35 @@ class PubmedJSONParser():
         print('Formatting data...', end='')
         output = {}
         text_type = self.data['documents'][0]['passages'][0]['text']
+        curr_section_type = self.data['documents'][0]['passages'][0]['infons']['section_type']
         for passage in self.data['documents'][0]['passages']:
             if passage['infons']['section_type'] == 'TITLE':
                 continue
 
             section_type = passage['infons']['section_type']
             if section_type not in output.keys():
+                # print('Creating new section...')
                 output[section_type] = {}
                 text_type = passage['infons']['type']
                 output[section_type][text_type] = []
-            #print('*'*100)
-            #print(passage['infons']['section_type'])
-            #print(passage['infons']['type'])
-            #print(passage['text'])
+
+            # In the event where the section has shown up before
+            elif section_type != curr_section_type:
+                text_type = passage['infons']['type']
+
+            # print('*'*100)
+            # print(passage['infons']['section_type'])
+            # print(passage['infons']['type'])
+            # print(passage['text'])
 
             if 'title' in passage['infons']['type'].lower():
                 output[section_type][passage['text']] = []
                 text_type = passage['text']
-                #print('text type changed here to:', text_type)
+                # print('text type changed here to:', text_type)
             else:
                 output[section_type][text_type].append(passage['text'])
-                
+
+            curr_section_type = section_type
         self.output = output
         print('Done!')
         
@@ -55,4 +68,10 @@ class PubmedJSONParser():
                     
             for key in keys_to_remove:
                 del val[key]
-        print('Done!')            
+        print('Done!')
+        
+    def save_to_file(self):
+        print('Saving formatted JSON to ' + self.output_path + '...', end='')
+        with open(self.output_path + '/' + self.pmc_id + '.json', 'w') as fp:
+            json.dump(self.output, fp)
+        print('Done!')
